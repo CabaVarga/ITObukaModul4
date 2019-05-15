cbee1c5797a1f5631232c9bb601b9c4037e9ee52
Caba Varga <caba.varga@outlook.com>
2019-05-16 00:50:09 +0200

    Primer 1.1 Zadaci 1-3

---
+++ Dan01/Dan01/Controllers/UtilController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace Dan01.Controllers
+{
+    public class UtilController : ApiController
+    {
+        // GET: util/date
+        [Route("util/date")]
+        [HttpGet]
+        public string CurrentDate()
+        {
+            return DateTime.Now.ToString(System.Globalization.DateTimeFormatInfo.InvariantInfo);
+        }
+
+        // GET: util/family
+        [Route("util/family")]
+        [HttpGet]
+        public List<string> MyFamily()
+        {
+            return new List<string>() { "Zoltan", "Rozalija", "Denes" };
+        }
+
+        // GET: util/myclass
+        [Route("util/myclass")]
+        [HttpGet]
+        public HttpResponseMessage MyClass()
+        {
+            var response = Request.CreateResponse();
+
+            string[,] grupa = { 
+                {"Goran", "Alasov"}, {"Aleksandar", "Đorđević"}, {"Radovan", "Josimović"},
+                {"Zoran", "Mandić"}, {"Svetozar", "Milinković"}, {"Marina", "Petrović"},
+                {"Dejan", "Pavlović"}, {"Dragana", "Sekulić"}, {"Uroš", "Štrbac"},
+                {"Marijana", "Tatalović"}, {"Čaba", "Varga"}, {"Viktor", "Vereš"},
+                {"Brane", "Vujković"}, {"Borisav", "Ignjatov"}, {"Boban", "Janković"},
+                {"Borka", "Lazarević"}, {"Srđan", "Savić"}, {"Nemanja", "Stojković"},
+                {"Danijel", "Petrović"}, {"Maja", "Ristić"}
+            };
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<html>\n\t<head>\n\t\t<title>Moja grupa</title>\n\t</head>\n\t<body>");
+            sb.Append("\n\t\t<table>\n\t\t\t<tr>\n\t\t\t\t<th>Broj</th>\n\t\t\t\t<th>Ime</th>\n\t\t\t\t<th>Prezime</th>\n\t\t\t</tr>");
+    
+            int broj = 1;
+            for (int i = 0; i < grupa.GetLength(0); i++)
+            {
+                sb.AppendFormat("\n\t\t\t<tr>\n\t\t\t\t<td>{0}</td>\n\t\t\t\t<td>{1}</td>\n\t\t\t\t<td>{2}</td>\n\t\t\t</tr>", broj, grupa[i, 0], grupa[i, 1]);
+                sb.AppendLine();
+                broj++;
+            }
+
+            sb.Append("\n\t\t</table>\n\t</body>\n</html>");
+
+            response.Content = new StringContent(sb.ToString(), new UTF8Encoding());
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            response.Content.Headers.ContentType.CharSet = "utf-8";
+
+            return response;
+        }
+    }
+}
---
+++ Dan01/Dan01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dan01.WebApiApplication" Language="C#" %>
