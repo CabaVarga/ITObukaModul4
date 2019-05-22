16f3b494f99be9bfee251c2f97620ffcb6ee4724
Caba Varga <caba.varga@outlook.com>
2019-05-22 18:08:38 +0200

    Novi da

---
+++ Dan03/Dan03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
