88793abdc3b502d9213b56b9b8d4a380a5844c1a
Caba Varga <caba.varga@outlook.com>
2019-05-30 00:03:15 +0200

    Dan05 Initial commit

---
+++ Dan05/data_access/Models/DataAccessContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace data_access.Models
+{
+    public class DataAccessContext : DbContext
+    {
+        public DataAccessContext() : base("DataAccessConnection")
+        {
+            Database.SetInitializer<DataAccessContext>(
+                new DropCreateDatabaseIfModelChanges<DataAccessContext>());
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
